9fdc239370b2f4c2227c7838ab35ade79d4f2229
Vansh Pratap Singh <168619471+AgentElemental@users.noreply.github.com>
2026-09-06 12:31:17 +0530

    Updated

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
 }
